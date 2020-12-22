b2a8e2a60397a44066d26a55e5f286f1d72825de
Canmarkyigitbas <76444119+Canmarkyigitbas@users.noreply.github.com>
2020-12-22 23:57:12 +0300

    Add files via upload

---
+++ GlobalAIHUB Homework1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+value1=int(input("Integer"))
+print("value1 :  {}, type1:{}".format(value1,type(value1)))
+
+
+# In[2]:
+
+
+value2=float(input("Float"))
+print("value2:{},type2:{}".format(value2,type(value2)))
+
+
+# In[5]:
+
+
+value3=str(input("String"))
+print("value3:{},type3:{}".format(value3,type(value3)))
+
+
+# In[9]:
+
+
+value4=bool(input("Boolean"))
+print("value4:{},type4:{}".format(value4,type(value4)))
+
+
+# In[13]:
+
+
+value5=str(input("String"))
+print("value5:{},type5:{}".format(value5,type(value5)))
+
+
+# In[ ]:
+
+
+
+
